475630ceaee59f205c6b46737d4dd43faa2ef0ad
LartSantos <larteosdsantos@gmail.com>
2026-02-26 08:30:02 -0300

    Add files via upload

---
+++ bool.py
@@ -0,0 +1,13 @@
+#resp = input("Você vai passar de ano? Yes/No")
+#resultado = bool(resp)
+
+#print("Resposta", resp)
+#print("Resultado", resultado)
+#pratic(resultado)
+
+resp = input("Você vai passar de ano? s/n")
+strip().lower()
+
+resultado = (resp == "S")
+print(type(resultado))
+
---
+++ entrada.py
@@ -0,0 +1,8 @@
+nome = input("Digite seu nome: ")
+idade = int(input("Digite sua idade: "))
+ira = float(input("Digite seu IRA :"))
+
+print(type(nome))
+print(type(idade))
+print(type(ira))
+
