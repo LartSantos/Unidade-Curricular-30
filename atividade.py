590cec07b9f24738d4fcad02437cb6752e11e722
LartSantos <larteosdsantos@gmail.com>
2026-03-04 15:56:39 -0300

    Add files via upload

---
+++ atividade.py
@@ -0,0 +1,9 @@
+compras = ["Arroz", "Feijão", "Leite pó", "Beterraba", "Fini"]
+
+print("Lista inicial:", compras)
+
+novo_item = input("Digite um item para adicionar à lista de compras:")
+
+compras.append(novo_item)
+
+print("Lista atualizada:", compras)
---
+++ lista2.py
@@ -0,0 +1,29 @@
+animal = ["Cachorro", "Gato"]
+print("Lista inicial:", animal)
+
+animal.append("Pato") #adiciona no fim da lista
+print("Lista atualizado:", animal)
+
+animal.insert(1, "Coelho") #add em uma posição específica
+print("Lista atualizado:", animal)
+
+animal.extend(["macaco", "leão"]) #add mais de um
+print("Lista atualizado: ", animal)
+
+animal.remove("leão")
+print(animal)
+
+removido = animal.pop()
+print(f"Removido: {removido}")
+print("Após pop()", animal)
+
+removido2 = animal.pop(1)
+print(f"Removido do índice 1 {removido2}")
+print("Após pop(1):", animal)
+
+del animal[0]
+print("Após o del ", animal)
+
+animal.clear()
+print(animal)
+
