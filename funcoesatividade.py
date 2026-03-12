1e52201a404ec6eb805691460fa182350e01e690
LartSantos <larteosdsantos@gmail.com>
2026-03-12 08:31:34 -0300

    Add files via upload

---
+++ funcoesatividade.py
@@ -0,0 +1,13 @@
+def calcular(numero1, numero2):
+
+    soma = numero1 + numero2
+
+    produto = numero1 * numero2
+
+    
+    print("A soma desses números é:", soma)
+    print("O produto desses números é:", produto)
+
+
+
+calcular(5, 4)
